3aa5eb278d00052c11874e7824efaac7d3582228
Michaël Schuh <michael.schuh.34@gmail.com>
2018-02-03 23:59:48 +0100

    update rules: en passant

--- elements.py
+++ elements.py
@@ -1,9 +1,12 @@
 from enum import Enum
+from collections import OrderedDict
+
 
 def char_range(c1, c2):
     "Generates the characters from c1 to c2 inclusive"
     for i in [chr(j) for j in range(ord(c1), ord(c2) + 1)]:
         yield i
+
 
 class Color(Enum):
     BLACK = 1
@@ -14,6 +17,7 @@ class Color(Enum):
 
     def opponent(color):
         return Color.BLACK if color is Color.WHITE else Color.WHITE
+
 
 class Square:
     def __init__(self, board, color, coords):
@@ -26,12 +30,23 @@ class Square:
         "Check whether a square is controlled by a set"
         return set.controls(self, excludeKing)
 
+    def removePiece(self):
+        if not self.piece:
+            return False
+
+        self.piece.remove()
+        self.piece = None
+
+        return True
+
+
 class Chessboard:
     def __init__(self):
         self.squares = {}
         for i in char_range('a', 'h'):
             for j in char_range('1', '8'):
                 self.squares[(i, j)] = Square(self, (ord(i) + int(j) + 1) % 2, (i, j))
+
 
 class Piece:
     def __init__(self, game, color, coords):
@@ -68,7 +83,7 @@ class Piece:
         column = chr(ord(self.square.coords[0] if origin is None else origin[0]) + move[0])
         row = str(int(self.square.coords[1] if origin is None else origin[1]) + move[1])
 
-        # return False if new_coord do n
+        # return False if new_coord do not exist
         if 'abcdefgh'.find(column) == -1 or '12345678'.find(row) == -1:
             return False
 
@@ -94,6 +109,10 @@ class Piece:
         list.append(newCoords)
 
         return self.recursiveAddCoords(list, newCoords, move, control)
+
+    def remove(self):
+        self.square = None
+        self.player().remove(self)
 
 
 class King(Piece):
@@ -208,9 +227,11 @@ class King(Piece):
 
         return move
 
+
 class Queen(Piece):
     def possibleMoves(self, control = False):
         return Rook.possibleMoves(self, control) + Bishop.possibleMoves(self, control)
+
 
 class Bishop(Piece):
     def possibleMoves(self, control = False):
@@ -224,6 +245,7 @@ class Bishop(Piece):
         possibleMoves = [ self.board.squares[coords] for coords in possibleMoves ]
 
         return possibleMoves
+
 
 class Knight(Piece):
     def possibleMoves(self, control = False):
@@ -268,21 +290,26 @@ class Rook(Piece):
 
         return possibleMoves
 
+
 class Pawn(Piece):
     def possibleMoves(self, control = False):
         coords = self.square.coords
         possibleMoves = []
         direction = 1 if self.color is Color.WHITE else -1
 
-        up = self.newCoords((0, 1 * direction))
+        # forward squares and "en passant" are not controlled
+        if not control:
+            forward = self.newCoords((0, 1 * direction))
 
-        if up and self.board.squares[up].piece is None:
-            possibleMoves.append(self.board.squares[up])
+            if forward and self.board.squares[forward].piece is None:
+                possibleMoves.append(self.board.squares[forward])
 
-        up2 = self.newCoords((0, 2 * direction))
+            forward2 = self.newCoords((0, 2 * direction))
 
-        if self.nbMoves == 0 and up2 and self.board.squares[up2].piece is None:
-            possibleMoves.append(self.board.squares[up2])
+            if self.nbMoves == 0 and forward2 and self.board.squares[forward2].piece is None:
+                possibleMoves.append(self.board.squares[forward2])
+
+            possibleMoves += self.enPassantMoves()
 
         for move in [(-1, 1 * direction), (1, 1 * direction)]:
             newCoords = self.newCoords(move)
@@ -290,21 +317,52 @@ class Pawn(Piece):
                 (control or self.board.squares[newCoords].piece.color != self.color):
                 possibleMoves.append(self.board.squares[newCoords])
 
+        return list(set(possibleMoves)) # deduplicate
+
+
+    def enPassantMoves(self):
+        possibleMoves = []
+
+        lastMove = self.game.lastMove()
+        if not lastMove:
+            return possibleMoves
+
+        for beside in [(-1, 0), (1, 0)]:
+            beside = self.newCoords(beside)
+
+            if not beside:
+                continue
+
+            beside = self.board.squares[beside]
+            # print(lastMove, beside)
+            if beside.piece and beside.piece.color is Color.opponent(self.color) \
+                and type(beside.piece).__name__ is 'Pawn' and lastMove.piece is beside.piece \
+                and lastMove.origin[1] is self.opponent().pawnsLine:
+                possibleMoves.append( \
+                    self.board.squares[(beside.coords[0], '3' if self.opponent().color is Color.WHITE else '6')])
+
         return possibleMoves
 
     def moveTo(self, coords):
+        enPassantMoves = self.enPassantMoves()
+
         move = Piece.moveTo(self, coords)
+
+        if self.square in enPassantMoves:
+            self.board.squares[self.newCoords((0, -1 if self.color is Color.WHITE else 1))].removePiece()
 
         if self.square.coords[1] in ['1', '8']:
             "promote"
 
         return move
 
+
 class Player:
     def __init__(self, game, color):
         "initialize a new set at the beginning of a game"
         self.game = game
         self.color = color
+
         self.piecesLine = '1' if color is Color.WHITE else '8' # whites => 1, blacks => 8
         self.pieces = []
         self.pieces.append (King (game, color, ('e', self.piecesLine)))
@@ -315,6 +373,8 @@ class Player:
         self.pieces.append (Knight (game, color, ('g', self.piecesLine)))
         self.pieces.append (Rook (game, color, ('a', self.piecesLine)))
         self.pieces.append (Rook (game, color, ('h', self.piecesLine)))
+
+        self.removed = []
 
         self.pawnsLine = '2' if color is Color.WHITE else '7' # whites => 2, blacks => 7
         self.pawns = []
@@ -347,10 +407,18 @@ class Player:
         return square in self.controlledSquares(excludeKing)
 
     def remove(self, piece):
+        self.removed.append(piece)
         self.pieces.remove(piece)
 
     def opponent(self):
         return self.game.players[Color.opponent(self.color)]
+
+
+class Move:
+    def __init__(self, piece, origin, destination):
+        self.piece = piece
+        self.origin = origin
+        self.destination = destination
 
 
 class Game:
@@ -361,6 +429,8 @@ class Game:
         self.players[Color.WHITE] = Player(self, Color.WHITE)
         self.players[Color.BLACK] = Player(self, Color.BLACK)
         self.hasToMove = Color.WHITE
+        self.moves = OrderedDict()
+        self.nbMoves = 1
 
     def move(self, origin, destination):
         piece = self.board.squares[origin].piece
@@ -374,7 +444,12 @@ class Game:
         if not piece.moveTo(destination):
             raise ValueError('movement not allowed')
 
+        self.moves[(self.nbMoves, self.hasToMove)] = Move (piece, origin, destination)
+
         self.hasToMove = Color.opponent(self.hasToMove)
+
+        if self.hasToMove is Color.WHITE:
+            self.nbMoves += 1
 
     def currentPlayerInCheck(self):
         "Check whether the current player in check"
@@ -382,3 +457,6 @@ class Game:
 
     def currentPlayerCheckmated(self):
         return self.players[self.hasToMove].checkmated()
+
+    def lastMove(self):
+        return self.moves[list(self.moves)[-1]] if len(self.moves) > 0 else False
